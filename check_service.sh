e52d036e702094985ece9c18b872e98bad7a9800
jianyong.jiang <jjy_8888@163.com>
2018-03-29 11:23:20 +0800

    fix erro

--- check_service.sh
+++ check_service.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 name=$1
-exist=$(sudo service name status 2>&1 | grep "unrecognized")
+exist=$(sudo service $name status 2>&1 | grep "unrecognized")
 if [ $exist != "" ];then
 	echo "Error: Service Not Found"
 else
-	stopped=$(sudo service name status | grep "stopped")
+	stopped=$(sudo service $name status | grep "stopped")
 	if [ $stopped != "" ];then
 		echo "Restarting"
-		sudo service name start
+		sudo service $name start
 	else
 		echo "is Running"
 	fi
