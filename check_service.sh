ee930f64aa7f124db15c6bc2d8f045e7958a5262
jianyong.jiang <jjy_8888@163.com>
2018-03-29 11:06:54 +0800

    add service check

---
+++ check_service.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+name=$1
+exist=$(sudo service name status 2>&1 | grep "unrecognized")
+if [ $exist != "" ];then
+	echo "Error: Service Not Found"
+else
+	stopped=$(sudo service name status | grep "stopped")
+	if [ $stopped != "" ];then
+		echo "Restarting"
+		sudo service name start
+	fi
+	else
+		echo "is Running"
+	fi
+fi
