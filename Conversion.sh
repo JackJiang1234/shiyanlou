16a83b50a993ca970e97a70f4f36101f5fbf0eed
jianyong.jiang <jjy_8888@163.com>
2018-03-30 13:55:21 +0800

    add conversion

---
+++ Conversion.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+input=$1
+
+function Convert(){
+	bytelimit = 1024
+	kblimit = ((1024*bytelimit))
+	mblimit = ((1024*kblimit))
+	
+	if [ $input -lt $bytelimit ];then
+		echo "${input}B"
+	elif [ $input -lt $kblimit ];then
+		echo "$((input/bytelimit))KB" 
+	elif [ $input -lt $mblimit ];then
+		echo "$((input/kblimit))MB"
+	else
+		echo "$((input/mblimit))GB"
+	fi
+}
+
+export -f Convert
