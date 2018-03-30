b3c204c5dfe49e555d8457e785e3945ccc3dae8c
jianyong.jiang <jjy_8888@163.com>
2018-03-30 14:20:43 +0800

    fix conversion

--- Conversion.sh
+++ Conversion.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-input=$1
-
 function Convert(){
+	input=$1
 	bytelimit=1024
-	kblimit=((1024*bytelimit))
-	mblimit=((1024*kblimit))
+	((kblimit=1024*bytelimit))
+	((mblimit=1024*kblimit))
 	
 	if [ $input -lt $bytelimit ];then
 		echo "${input}B"
 	elif [ $input -lt $kblimit ];then
-		echo "$((input/bytelimit))KB" 
+		echo "$((input / bytelimit))KB" 
 	elif [ $input -lt $mblimit ];then
-		echo "$((input/kblimit))MB"
+		echo "$((input / kblimit))MB"
 	else
-		echo "$((input/mblimit))GB"
+		echo "$((input / mblimit))GB"
 	fi
-}
+}git 
 
 export -f Convert
+
+Convert $1
